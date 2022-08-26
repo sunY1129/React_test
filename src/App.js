6bb5484b7bd42843181cb44dffda65a2ba8cea0c
sunY1129 <sla7821@naver.com>
2022-08-26 19:16:10 +0900

    sample 복습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
 function App() {
+  var a = 10 + 10
+  console.log(a)
+  var name = 'MyName'
+  console.log('안녕하세요'+name)
+  const text = <h1>React Project,{10 * 20}</h1>
+  function add10(num){
+    return num + 10
+  }
+  function handleClickEvent(e){
+    alert(e.target.textContent)
+  }
+  const customStyle={
+    color:'red', textAlign:'center'
+  }
+  class MyTag extends React.Component{
+    render(){
+      return(
+        <div>Hello, {this.props.name}</div>
+      )
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={customStyle}>Main test</h1>
+      <h2 style={{color:'blue',backgroundColor:'yellow'}}>Sub test</h2>
+      <div>test</div>
+      <MyTag name="park"/>
+      <div>
+        {a}
+        {text}
+        <h1 className='title' onClick={handleClickEvent}>
+          add10의 함수 결과: {add10(3)}
+        </h1>
+      </div>
     </div>
   );
 }
